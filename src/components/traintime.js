3e8e21d831caa03c27543246681854db0d4f856b
Dheep Balaraman <dheep.balaraman@kineticit.com.au>
2021-03-07 16:46:01 +1100

    intrim commit

---
+++ src/components/traintime.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class traintime extends Component {
+
+    state = {
+        mytraintime: {}
+    }
+
+    componentDidMount() {
+        
+    }
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default traintime
